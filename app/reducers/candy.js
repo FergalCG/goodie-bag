af7fbd9783e11c63238dd1f51eebd4c245ad8600
FergalCG <https://FergalCG@github.com>
2019-08-26 13:51:35 -0400

    second try

---
+++ app/reducers/candy.js
@@ -0,0 +1,17 @@
+
+const GET_CANDY = 'GET_CANDY'
+
+const gotCandy = () => ({
+    type: GET_CANDY
+})
+
+const getCandy = ()
+
+const initialState = []
+
+const candyReducer = (state = initialState, action) => {
+    switch(action.type) {
+        default:
+            return state
+    }
+}
--- server/db/models/Candy.js
+++ server/db/models/Candy.js
@@ -1,6 +1,31 @@
 const Sequelize = require('sequelize');
 const db = require('../database');
 
-module.exports = db.define('candy', {
-  // define your model here!
+const Candy = db.define('candy', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+    description: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+    quantity: {
+        type: Sequelize.INTEGER,
+        validate: {
+            min: 0,
+            max: 10
+        }
+    },
+    imgUrl: {
+        type: Sequelize.TEXT
+    }
 });
+
+module.exports = Candy 
